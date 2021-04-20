8521ab9215049d6b545b014f6e2b8042e0a915e6
yukimi.y <yyy.yukimi@gmail.com>
2021-04-20 18:32:22 +0900

    修正

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class RestaurantController < ApplicationController
+    class RestaurantsController < ApplicationController
       def index
         restaurants = Restaurant.all
         render json: {
