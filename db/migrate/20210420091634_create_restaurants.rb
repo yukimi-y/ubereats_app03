54699d3732e7560f3758e9be10ea0c0a0a4dcd27
yukimi.y <yyy.yukimi@gmail.com>
2021-04-20 18:19:30 +0900

    rails db:migrate実行

--- db/migrate/20210420091634_create_restaurants.rb
+++ db/migrate/20210420091634_create_restaurants.rb
@@ -1,6 +1,9 @@
-class CreateRestaurants < ActiveRecord::Migration[6.1]
+class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
+      t.string :name, null: false
+      t.integer :fee, null: false, default: 0
+      t.integer :time_required, null: false
 
       t.timestamps
     end
