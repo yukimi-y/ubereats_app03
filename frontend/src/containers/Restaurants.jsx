2c8313138a658e0abb888bc336df8f016cef6846
yukimi.y <yyy.yukimi@gmail.com>
2021-05-01 07:37:40 +0900

    APIデータを画面に表示

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,8 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
+
+// reducers
+import {
+  initialState,
+  restaurantsActionTypes,
+  restaurantsReducer,
+} from '../reducers/restaurants';
 
 // images
 import MainLogo from '../images/logo.png';
@@ -27,10 +34,18 @@ const MainCover = styled.img`
 `;
 
 export const Restaurants = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
   useEffect(() => {
+    dispatch({ type: restaurantsActionTypes.FETCHING });
     fetchRestaurants()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: restaurantsActionTypes.FETCH_SUCCESS,
+          payload: {
+            restaurants: data.restaurants
+          }
+        })
       )
   }, [])
 
@@ -42,6 +57,13 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      {
+        state.restaurantsList.map(restaurant =>
+          <div>
+            {restaurant.name}
+          </div>
+        )
+      }
     </Fragment>
   )
 }
